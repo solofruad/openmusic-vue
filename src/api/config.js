7cee9539da80ab9a252fc22c1f24d79646283ec4
solofruad <nestorsuat>
2018-05-03 23:36:10 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+//Application name 	openmusic
+//API key 	a4f4f0b6e7183cc11ed534a5baefc970
+//Shared secret 	4149913a751eda326f1d31282bd33794
+//Registered to 	nestorsuat
+
+export default{
+	appName: 'openmusic',
+	apiKey: 'a4f4f0b6e7183cc11ed534a5baefc970',
+	secret: '4149913a751eda326f1d31282bd33794',
+	registeredTo: 'nestorsuat'
+}
