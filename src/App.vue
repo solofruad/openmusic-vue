0af4edc1231c21dc43e7bd58e96be846cfb786e3
solofruad <nestorsuat>
2018-05-03 23:48:11 -0500

    Fixes URL logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='https://solofruad.github.io/openmusic-vue/dist/logo.png')
+    img(src='dist/logo.png')
     h1 OpenMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" :value="country.value") {{ country.name }}
